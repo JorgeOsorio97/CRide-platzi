73b8463be453c7efe7369223018dcf563f1da2e4
JorgeOsorio97 <jorge.valdez.osorio@gmail.com>
2020-01-02 09:37:38 -0600

    setup para arrancar basado en cride de platzi

---
+++ cride/utils/models.py
@@ -0,0 +1,30 @@
+""" Django.models.utilities"""
+
+# Django
+from django.db import models
+
+class CRideModel(models.Model):
+    """ Comparte Ride base model.
+    CRideModel acts as an abstract base class from which every
+    other model in the project will inherit.
+    This class provides every table with the followint attributes:
+        * Created (DateTime): The datetime when model was created.
+        * modified (DateTime): FalseThe last datetime the object was modified.
+    """
+
+    created = models.DateTimeField(
+        'created at',
+        auto_now_add=True,
+        help_text='DateTime on which the object was created'
+    )
+    modified = models.DateTimeField('modified at',
+        auto_now=False,
+        help_text='Datetime on which the object was las modified'
+    )
+
+    class Meta:
+        """ Meta options """
+        abstract = True
+        
+        get_latest_by = "created"
+        ordering = ['-created', '-modified']
