1aec2e0bce7405c0bf9f441cac7dfc63befa570e
JorgeOsorio97 <jorge.valdez.osorio@gmail.com>
2020-01-16 23:24:13 -0600

    agregar Django REST Framewokr (DRF)

---
+++ cride/circles/views.py
@@ -0,0 +1,27 @@
+"""Circles Views"""
+
+# Django REST framework
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# Models
+from cride.circles.models import Circle
+
+# Serializers
+from cride.circles.serializers import CircleSerializer
+from cride.circles.serializers import CreateCircleSerializer
+
+@api_view(['GET'])
+def list_circles(request):
+    """List Circles."""
+    circles = Circle.objects.filter(is_public=True)
+    serializer = CircleSerializer(circles, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def create_circle(request):
+    """Create circle"""
+    serializer = CreateCircleSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    circle = serializer.save()
+    return Response(CircleSerializer(circle).data)
